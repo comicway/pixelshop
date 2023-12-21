0ba1d494243f6bc03dde5137151da0452f2a6ddf
comicway <129238200+comicway@users.noreply.github.com>
2023-12-20 21:11:11 -0300

    NavBar e inicio de CartWidget

---
+++ react-pixelshop/src/components/NavBar/navbar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <div class="nav-scroller py-1 mb-3 border-bottom">
+            <nav class="nav nav-underline justify-content-between">
+            <a class="nav-item nav-link link-body-emphasis active" href="#">Tienda</a>
+            <a class="nav-item nav-link link-body-emphasis" href="#">Ofertas</a>
+            <a class="nav-item nav-link link-body-emphasis" href="#">Blog</a>
+            <a class="nav-item nav-link link-body-emphasis" href="#">Mi cuenta</a>
+            <a class="nav-item nav-link link-body-emphasis" href="#">Contacto</a>
+            </nav>
+        </div>
+    )
+};
+export default NavBar
--- react-pixelshop/src/main.jsx
+++ react-pixelshop/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/navbar';
+import CartWidget from './components/CartWidget/cartwidget';
 
 
 const rootDivElement = document.getElementById('root');
@@ -8,28 +10,25 @@ const rootDivElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootDivElement);
 
 function App(){
-    return <div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <div class="col-md-3 mb-2 mb-md-0">
-        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-          <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-        </a>
+    return (
+        <div class="container">
+  <header class="border-bottom lh-1 py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-4 pt-1">
+        <a class="link-secondary" href="#">Subscribe</a>
       </div>
-
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2">Features</a></li>
-        <li><a href="#" class="nav-link px-2">Pricing</a></li>
-        <li><a href="#" class="nav-link px-2">FAQs</a></li>
-        <li><a href="#" class="nav-link px-2">About</a></li>
-      </ul>
-
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
+      <div class="col-4 text-center">
+        <a class="blog-header-logo text-body-emphasis text-decoration-none" href="#">Comic Way</a>
       </div>
-    </header>
-  </div>
+      <div class="col-4 d-flex justify-content-end align-items-center">
+        <CartWidget/>
+        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+      </div>
+    </div>
+  </header>
+        <NavBar/>
+</div>
+    )
 };
 
 root.render(<App/>);
